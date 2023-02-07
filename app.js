cf535f7cc635882309fc3570b5f5dd887aaee89b
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-07 16:58:09 +0100

    iniciando lab

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('Hello, world!');
+  response.end();
+});
+
+server.listen(3000);
